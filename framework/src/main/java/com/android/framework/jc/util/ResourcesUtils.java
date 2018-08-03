59ee8ae59bcf342cd5b4b517a55b3a1f97652ca6
hujc <13365755735@163.com>
2018-08-03 18:18:38 +0800

    新增FragmentPagerAdapter封装

--- framework/src/main/java/com/android/framework/jc/util/ResourcesUtils.java
+++ framework/src/main/java/com/android/framework/jc/util/ResourcesUtils.java
@@ -1,6 +1,7 @@
 package com.android.framework.jc.util;
 
 import android.content.res.Resources;
+import android.support.annotation.StringRes;
 import android.util.DisplayMetrics;
 
 import com.android.framework.jc.JcFramework;
@@ -18,5 +19,9 @@ public class ResourcesUtils {
 
     public static Resources getResource(){
         return JcFramework.getInstance().getApplication().getResources();
+    }
+
+    public static String getString(@StringRes int stringRes){
+        return getResource().getString(stringRes);
     }
 }
