36d038c1198a843318ac5799736187a915a101ad
hujc <13365755735@163.com>
2018-09-11 14:25:24 +0800

    去掉无用代码

--- framework/src/main/java/com/android/framework/jc/recyclerview/expend/ExpendBean.java
+++ framework/src/main/java/com/android/framework/jc/recyclerview/expend/ExpendBean.java
@@ -1,14 +1,14 @@
-package com.android.framework.jc.recyclerview.expend;
-
-import java.util.List;
-
-/**
- * @author Mr.Hu(Jc) JCFramework
- * @create 2018/9/5 14:34
- * @describe
- * @update
- */
-public class ExpendBean<G, T> {
-    private G groupBean;
-    private List<T> itemList;
-}
+//package com.android.framework.jc.recyclerview.expend;
+//
+//import java.util.List;
+//
+///**
+// * @author Mr.Hu(Jc) JCFramework
+// * @create 2018/9/5 14:34
+// * @describe
+// * @update
+// */
+//public class ExpendBean<G, T> {
+//    private G groupBean;
+//    private List<T> itemList;
+//}
